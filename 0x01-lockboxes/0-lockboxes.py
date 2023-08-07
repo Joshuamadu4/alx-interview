76ef28b7f3606e5bfca19be0193deda52874fe9d
joshuamadu4 <joshuamadu4@gmail.com>
2023-08-06 18:32:13 -0700

    Corrected

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,44 +3,40 @@
    from a list of lists
 """
 
-def can_unlock_all(boxes=None):
-    """A function that returns True if all boxes in
-    boxes can be opened
+
+def canUnlockAll(boxes=[]):
+    """A function that returns True of all box in
+    boxes can be opend
     """
     if not boxes:
         return False
 
-    # Initialize a set to keep track of keys available
-    keys = {0}
-
-    # Iterate through each box and its corresponding index
+    keys = set([0])
     for box_id, box in enumerate(boxes):
         for key in box:
-            # Check if the key is valid and not the current box
             if key < len(boxes) and key != box_id:
                 keys.add(key)
 
-    # If the number of unique keys matches the number of boxes, return True
     if len(keys) != len(boxes):
         return False
 
     return True
 
+
 if __name__ == '__main__':
     boxes = [
-        [1, 3],
-        [2],
-        [3, 0],
-        [1, 2, 3],
-    ]
-    print(can_unlock_all(boxes))
+                [1, 3],
+                [2],
+                [3, 0],
+                [1, 2, 3],
+            ]
+    print(unlockBoxes(boxes))
 
     boxes = [[1], [2], [3], [4], []]
-    print(can_unlock_all(boxes))
+    print(canUnlockAll(boxes))
 
     boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(can_unlock_all(boxes))
+    print(unlockBoxes(boxes))
 
     boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(can_unlock_all(boxes))
-
+    print(unlockBoxes(boxes))
